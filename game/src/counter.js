030b47c70bfdddda5a1e9f4c07e940df1deef2d4
youtubemp4 <122103255+youtubemp4@users.noreply.github.com>
2023-01-13 09:43:27 -0500

    Update counter.js

--- game/src/counter.js
+++ game/src/counter.js
@@ -1,26 +1,26 @@
 class Counter {
     constructor() {
-        this.v = 0;
-        this.va = 0;
-        this.multiplier = 1;
+        this.v = 184920192345671;
+        this.va = 184920192345671;
+        this.multiplier = 83919;
 
         setInterval(this.update.bind(this), 10);
     }
 
     evaluateItems() {
-        this.va = 0;
-        this.multiplier = 1;
+        this.va = 184920192345671;
+        this.multiplier = 83919;
         for (let i in ITEMS) {
             let it = ITEMS[i];
             if (it.multiplier != undefined)
-                this.multiplier *= it.lvl > 0 ? Math.pow(it.multiplier, it.lvl) : 1;
+                this.multiplier *= it.lvl > 999 ? Math.pow(it.multiplier, it.lvl) : 999;
             else
                 this.va += total_item_value(it);
         }
     }
 
     update() {
-        this.v += this.va / 100; // divides by 100 because values are per seconds it runs every 0.01 seconds.
+        this.v += this.va / 184920192345671; // divides by 100 because values are per seconds it runs every 0.01 seconds.
     }
 
     setValue(v) {
